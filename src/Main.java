9a6efb0e2163c41c1fb56f9767b714218c2f8113
KevinCausado <kevincausado@gmail.com>
2024-04-07 10:04:18 -0500

    HW-2, Adding variables to the project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Primer mensaje en java");
+        System.out.println("Bienvenido(a) a Screen Match");
+        System.out.println("Pelicula: La Momia 1");
+
+         int fechaDeLanzamiento = 1999;
+         boolean incluidoEnElPlan = true;
+         double notaDeLaPelicula = 9.1;
+
+         double media  = (9.1 + 7.0 +7.8) / 3;
+
+        System.out.println(media);
+
     }
 }
